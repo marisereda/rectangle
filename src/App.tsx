9411215879cd4bef17f0371ca777e6bbd51109c8
Marina Sereda <marina.gorb@gmail.com>
2023-08-02 11:23:33 +0300

    add window resize event

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,34 @@ function App () {
   const targetRef = useRef<HTMLDivElement>(null)
   const containerRef = useRef<HTMLDivElement>(null)
 
+  const setTargetPosition = (x: number, y: number) => {
+    if (targetRef.current === null || containerRef.current === null) { return }
+    const maxX = containerRef.current.clientWidth - targetRef.current.clientWidth
+    const maxY = containerRef.current.clientHeight - targetRef.current.clientHeight
+
+    let newX = x < 0 ? 0 : x
+    let newY = y < 0 ? 0 : y
+    // if (newX < 0) { newX = 0 }
+    // if (newY < 0) { newY = 0 }
+
+    if (newX > maxX) { newX = maxX }
+    if (newY > maxY) { newY = maxY }
+
+    targetRef.current.style.top = `${newY}px`
+    targetRef.current.style.left = `${newX}px`
+    targetRef.current.style.transform = 'translate(0%,0%)'
+  }
+
+  useEffect(() => {
+    const onResize = () => {
+      if (targetRef.current === null) { return }
+      const currentX = targetRef.current.offsetLeft
+      const currentY = targetRef.current.offsetTop
+      setTargetPosition(currentX, currentY)
+    }
+    window.addEventListener('resize', onResize)
+  }, [])
+
   const startMoving = (e: React.MouseEvent) => {
     if (targetRef.current === null) { return }
     offset.current = { x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY }
@@ -16,19 +44,10 @@ function App () {
 
   const onMoving = (e: React.MouseEvent) => {
     if (!isMoving.current || targetRef.current === null || containerRef.current === null) { return }
-    const maxPositionX = containerRef.current.clientWidth - targetRef.current.clientWidth
-    const maxPositionY = containerRef.current.clientHeight - targetRef.current.clientHeight
-    let newPositionX = e.nativeEvent.offsetX - offset.current.x
-    let newPositionY = e.nativeEvent.offsetY - offset.current.y
+    const newPositionX = e.nativeEvent.offsetX - offset.current.x
+    const newPositionY = e.nativeEvent.offsetY - offset.current.y
 
-    if (newPositionX < 0) { newPositionX = 0 }
-    if (newPositionY < 0) { newPositionY = 0 }
-    if (newPositionX > maxPositionX) { newPositionX = maxPositionX }
-    if (newPositionY > maxPositionY) { newPositionY = maxPositionY }
-
-    targetRef.current.style.top = `${newPositionY}px`
-    targetRef.current.style.left = `${newPositionX}px`
-    targetRef.current.style.transform = 'translate(0%,0%)'
+    setTargetPosition(newPositionX, newPositionY)
   }
 
   const stopMoving = () => {
